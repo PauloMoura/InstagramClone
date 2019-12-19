62c2b864144eaea059eb08b5c16a00bc5fb002f0
paulomoura91 <pv.moura91@gmail.com>
2019-12-19 02:00:38 -0300

    Sign Up and Login Parse

--- app/src/main/java/com/paulomoura/instagramclone/SignUpActivity.java
+++ app/src/main/java/com/paulomoura/instagramclone/SignUpActivity.java
@@ -2,6 +2,7 @@ package com.paulomoura.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -80,7 +81,8 @@ public class SignUpActivity extends AppCompatActivity {
 
         btnNextActivity = findViewById(R.id.btnNextActivity);
         btnNextActivity.setOnClickListener(view -> {
-
+            Intent intent = new Intent(SignUpActivity.this, SignUpLoginActivity.class);
+            startActivity(intent);
         });
     }
 
